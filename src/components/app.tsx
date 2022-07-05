f4eaeece5566268989bbf44d806da795906ee9b0
Fadi Nasr <fadi1nasr@outlook.com>
2022-07-05 02:47:43 +0200

    dev(development): adding dev mode buttons to main page

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,10 +6,12 @@ import { EntitiesContext } from "../index";
 export const App = observer(() => {
   const entityStore = useContext(EntitiesContext);
 
+  // Check if the name of the new entity already exists
   const entityNameExists = (name: string) => {
     return entityStore.entities.find(entity => entity.name === name);
   }
 
+  // Add a new entity
   const onAddEntity = () => {
     const entityName = prompt("Name of the new entity", "");
     if (entityName && !entityNameExists(entityName)) {
@@ -20,10 +22,25 @@ export const App = observer(() => {
       );
     }
   };
+
+  // ------------- Start - Dev Only -------------------
+  // Save data on Canvas to local Storage (by hand - for testing purposes)
+  const onSaveDataToLocalStorage = () => {
+    entityStore.saveToLocalStorageReaction()
+  }
+
+  // Load data from mock Services
+  const onLoadFromLocalStorage = () => {
+    entityStore.loadMockData();
+  }
+  // ------------- End - Dev Only -------------------
+
   return (
     <div>
       <h1>Domain Model Editor</h1>
       <button onClick={onAddEntity}>Add Entity</button>
+      <button onClick={onSaveDataToLocalStorage}>Save to Local Storage</button>
+      <button onClick={onLoadFromLocalStorage}>Load from Mock Services</button>
       <EntityCanvas />
     </div>
   );
